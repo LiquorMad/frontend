3943129f57d537660d1d7a137d9ea6ffd61abe54
Liquor Mad <liquormads@gmail.com>
2023-09-30 10:55:45 -0100

    DropDownUser

--- components/layouts/DropDownUser.tsx
+++ components/layouts/DropDownUser.tsx
@@ -1,7 +1,8 @@
+import { LogOut, UserCircle2 } from 'lucide-react';
 import React from 'react'
+
 type DropDownUserProps ={
     visible: boolean,
-    onClose:()=>void
 }
 
 function DropDownUser({visible}:DropDownUserProps) {
@@ -9,11 +10,9 @@ function DropDownUser({visible}:DropDownUserProps) {
   return (
     <div className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" >
     <div className="py-1" role="none">
-      <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem"  >Account settings</a>
-      <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem"  >Support</a>
-      <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem"  >License</a>
+      <a href="#" className="text-gray-700 px-4 py-2 text-sm flex gap-3" role="menuitem" > <UserCircle2 />Profile</a>
       <form method="POST" action="#" role="none">
-        <button type="submit" className="text-gray-700 block w-full px-4 py-2 text-left text-sm" role="menuitem"  id="menu-item-3">Sign out</button>
+        <button type="submit" className="text-gray-700 flex w-full px-4 py-2 text-left text-sm gap-3" role="menuitem"  id="menu-item-3">    <LogOut />Sign out</button>
       </form>
     </div>
   </div>
--- components/layouts/Navbar.tsx
+++ components/layouts/Navbar.tsx
@@ -2,15 +2,14 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import DropDownUser from './DropDownUser';
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover"
-
 
 const Navbar = () => {
   const [modalOpen, setModalOpen] = React.useState(false);
+  
+  function handleVisibility(){
+    setModalOpen(!modalOpen)
+  }
+
   return (
   <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
     <ul className="bg sticky px-10 py-3  mx-auto flex items-certer justify-between">
@@ -28,15 +27,13 @@ const Navbar = () => {
       </li>
       <li>
         <Image
-          onClick={() => setModalOpen(true)}
-          onBlur={() => setModalOpen(false)}
+          onClick={handleVisibility}
           className="w-10 h-10 rounded-full cursor-pointer" 
           width={300} 
           height={200} 
           src="/images/f1.jpg" 
           alt="Rounded avatar"/>
-          <DropDownUser visible={modalOpen} onClose={() => setModalOpen(false) }/>
-          
+          <DropDownUser visible={modalOpen} />
       </li>
     </ul>
   </nav>
